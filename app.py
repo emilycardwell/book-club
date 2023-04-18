e5fedf163732400fcbdc12c1ca87a19e1c9b0c2c
Emily Cardwell <emily@emilycardwell.com>
2023-04-18 20:58:31 +0200

    unignore requirements and other code editing

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import os
 import streamlit as st
-import streamlit_sortables as ss
+from streamlit_sortables import sort_items
 import pyrankvote
 from pyrankvote import Candidate, Ballot
 
 # VARIABLES
-data_path = os.environ['DATA_PATH']
-file_name = 'April_ALE.csv'
-file_path = f'{data_path}/{file_name}'
+file_path = 'data/Feb2023.csv'
 
 # READ CSV & CLEAN
-feb_df = pd.read_csv(file_path)
-sorted_df = feb_df.sort_values('Book').reset_index(drop=True).loc[:, ['Book', 'Author']]
-sorted_df.head()
+def read_data(file_path):
+    with open(file_path, 'r') as file_path:
+        feb_df = pd.read_csv(file_path)
 
-items = []
-for key, data in sorted_df.iterrows():
-    items.append(f'{data[0]} - {data[1]}')
+    sorted_df = feb_df.sort_values('Book').reset_index(drop=True).loc[:, ['Book', 'Author']]
+
+    items = []
+    for key, data in sorted_df.iterrows():
+        items.append(f'{data[0]} - {data[1]}')
+
+    return (items)
+
+items = read_data(file_path)
 
 books = []
 for i in items:
@@ -34,7 +38,7 @@ st.markdown(
 )
 
 
-sorted_books = ss.sort_items(items, header=None, direction='vertical')
+sorted_books = sort_items(items, header=None, direction='vertical')
 
 submissions = 0
 ballots = []
