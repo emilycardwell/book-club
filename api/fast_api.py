53e33404475af2fe59d54ecae9257e55151201f1
Emily Cardwell <emily@emilycardwell.com>
2023-04-20 17:59:35 +0200

    working on api

--- api/fast_api.py
+++ api/fast_api.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import csv
 import pyrankvote as prv
 
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/add_ballot")
