aab9bf9b806960b4648017f71d829d1d1e638c70
Emily Cardwell <emily@emilycardwell.com>
2023-04-26 18:33:37 +0200

    finessing response to more readable format5

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def api_get_results():
     except:
         response = 'Input Error, try again'
 
-    return type(response[1])['name']
+    return type(response[1]), response[1]
 
 def api_clear_ballots():
     url = 'https://book-club-zkfrzn26zq-oa.a.run.app/clear_ballots'
